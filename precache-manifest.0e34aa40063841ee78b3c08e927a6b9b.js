b3dc66b593829453c784bc1ce3701babf6d458aa
smirnoffnew <smirnoffnew@gmail.com>
2019-09-05 15:28:03 +0300

    Updates

---
+++ precache-manifest.0e34aa40063841ee78b3c08e927a6b9b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9fd60857084cd7da41cc67a6b759a17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d2467601fac75f2fed4",
+    "url": "/static/js/2.eab92312.chunk.js"
+  },
+  {
+    "revision": "1636322b6b68aa1cd1e8",
+    "url": "/static/js/main.039b6632.chunk.js"
+  },
+  {
+    "revision": "a3dbc1909dd566eb235d",
+    "url": "/static/js/runtime~main.1f997180.js"
+  }
+]);
