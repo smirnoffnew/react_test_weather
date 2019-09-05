d933885f1926afff68e39c1bd981c9cf08bb7a76
smirnoffnew <smirnoffnew@gmail.com>
2019-09-05 16:19:13 +0300

    changed url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const API_KEY = 'ASWcvsmMjzKodukBPmM4NMJY6mFIRoA5';
-export const BASE_URL = 'http://dataservice.accuweather.com'
+export const BASE_URL = 'https://dataservice.accuweather.com'
 
 export const ADD_LOCATION = 'ADD_LOCATION';
 export const REMOVE_LOCATION = 'REMOVE_LOCATION';
